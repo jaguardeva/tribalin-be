31f206222373be15f9153102d35be3e9dd4913c5
Muhammad Ainurrofiq Anwar Buhang <87835637+RafiArt@users.noreply.github.com>
2024-12-05 11:24:05 +0700

    a

--- src/routes/transactionRoute.js
+++ src/routes/transactionRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
     getAccommodationBooking,
     getAccommodationBookingById,
-    createAccommodationBooking
+    createAccommodationBooking,
+    paymentCallback,
+    deleteAccommodationBooking
 } from "../controllers/transactionController.js";
 import { checkRole, verifyToken } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -21,6 +23,11 @@ router.post("/accommodationBooking/:id/booking",
     verifyToken,
     createAccommodationBooking
 );
+router.post("/accommodationBooking/paymentCallback", paymentCallback);
 
+router.delete("/accommodationBooking/:uuid",    // Route untuk delete
+    verifyToken, 
+    deleteAccommodationBooking  // Fungsi delete
+);
 
 export default router;
